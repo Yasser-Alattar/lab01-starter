33d7bb4811e4a0c59e11b15a9fea9f4a25c9e4b9
Yasser-Alattar <attaryasser479@gmail.com>
2025-12-31 21:24:25 +0200

    Initialize main.c with greeting program structure
    Added a basic structure for a greeting program.

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+
+    // TODO:
+    // 1. Ask the user for their name using scanf
+    // 2. Print "Hello, [Name]!" (e.g., "Hello, Bilal!")
+    
+    return 0;
+}
